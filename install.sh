5415ad553b14b2137c70c6d3b6aeee4e504d4940
ray <ray@eyeball.com>
2015-06-29 09:47:13 +0800

    adding a new module

--- install.sh
+++ install.sh
@@ -1,2 +1,2 @@
-npm install commander@2.8.1 node-xmpp-client@1.0.0-alpha20 node-xmpp-core@1.0.0-alpha14 sazzle@0.0.8
+npm install commander node-xmpp-client@1.0.0-alpha20 node-xmpp-core@1.0.0-alpha14 sazzle@0.0.8 node-stringprep
 for x in *.patch ; do patch -p0 <  $x; done
